f6107b3583a132d1b1d09a7a00441463a1d7286f
iamandaleao <amandabibiano144@gmail.com>
2025-10-17 15:20:58 -0300

    feat: add OOP practice challenges

---
+++ src/desafio5/Aluno.java
@@ -0,0 +1,14 @@
+package desafio5;
+
+//Crie uma classe Aluno com atributos nome, idade, e um métdo para exibir informações. Crie uma instância da classe Aluno, atribua valores aos seus atributos e utilize o métdo para exibir as informações.
+public class Aluno {
+    String nome;
+    int idade;
+
+    void exibeInformacoes() {
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Idade do aluno: " + idade);
+    }
+}
+
+
