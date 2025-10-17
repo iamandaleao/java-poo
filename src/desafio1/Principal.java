22a629692da6e2948fbf1165f671b056cf0c1e85
iamandaleao <amandabibiano144@gmail.com>
2025-10-17 15:21:51 -0300

    feat: add OOP practice challenges

---
+++ src/desafio1/Principal.java
@@ -0,0 +1,9 @@
+package desafio1;
+
+// Crie uma classe Pessoa com um métdo que exibe "Olá, mundo!" no console.
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.saudacao();
+    }
+}
