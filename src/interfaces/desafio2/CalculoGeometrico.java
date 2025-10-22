de8445d687d2557e2b6f949f98fc92211e5c2c98
iamandaleao <amandabibiano144@gmail.com>
2025-10-22 11:29:03 -0300

    feat: add new challenges

---
+++ src/interfaces/desafio2/CalculoGeometrico.java
@@ -0,0 +1,11 @@
+package interfaces.desafio2;
+
+// Interface CalculoGeometrico
+public interface CalculoGeometrico {
+    void calcularArea(double altura, double largura);
+    void calcularPerimetro(double altura, double largura);
+}
+
+
+
+
---
+++ src/interfaces/desafio2/TesteCalculadoraSalaRetangular.java
@@ -0,0 +1,10 @@
+package interfaces.desafio2;
+
+// Classe de Teste
+public class TesteCalculadoraSalaRetangular {
+    public static void main(String[] args) {
+        CalculadoraSalaRetangular calculadora = new CalculadoraSalaRetangular();
+        calculadora.calcularArea(5, 8); // Exemplo com altura e largura
+        calculadora.calcularPerimetro(5, 8);
+    }
+}
