a5d1d37c0ca8d180d1e7c2d2a612d19e6e0a2fbc
iamandaleao <amandabibiano144@gmail.com>
2025-10-24 09:59:53 -0300

    feat: add new challenges

--- src/desafio1/Pessoa.java
+++
@@ -1,8 +0,0 @@
-package desafio1;
-
-// Crie uma classe Pessoa com um métdo que exibe "Olá, mundo!" no console.
-public class Pessoa {
-    void saudacao() {
-        System.out.println("Olá, mundo!");
-    }
-}
--- src/desafio1/Principal.java
+++
@@ -1,9 +0,0 @@
-package desafio1;
-
-// Crie uma classe Pessoa com um métdo que exibe "Olá, mundo!" no console.
-public class Principal {
-    public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        pessoa.saudacao();
-    }
-}
