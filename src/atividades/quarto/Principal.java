85ce29d6fd46db01cf8f331fa33c4e64721efa8c
iamandaleao <amandabibiano144@gmail.com>
2025-10-30 11:38:50 -0300

    feat: add new challenges

---
+++ src/atividades/quarto/Principal.java
@@ -0,0 +1,25 @@
+package atividades.quarto;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Ar condicionado", 2000.0);
+        Produto produto2 = new Produto("Aquecedor", 800.0);
+        Produto produto3 = new Produto("Ventilador", 150.0);
+
+        ArrayList<Produto> listaProdutos = new ArrayList<>();
+        listaProdutos.add(produto1);
+        listaProdutos.add(produto2);
+        listaProdutos.add(produto3);
+
+        double somaPrecos = 0;
+        for (Produto produto : listaProdutos) {
+            somaPrecos += produto.preco();
+        }
+
+        double precoMedio = somaPrecos / listaProdutos.size();
+        System.out.println("Preço médio dos produtos: " + precoMedio);
+    }
+}
+
