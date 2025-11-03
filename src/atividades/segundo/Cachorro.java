a0bc44689a3d0dd4de60f06cfeb94e1a2763d779
iamandaleao <amandabibiano144@gmail.com>
2025-11-03 18:50:59 -0300

    feat: add new challenges

--- src/atividades/segundo/Cachorro.java
+++ src/atividades/segundo/Cachorro.java
@@ -1,20 +1,20 @@
-package atividades.segundo;
-// Crie uma classe Animal e uma classe Cachorro que herda de Animal.
-// Em seguida, crie um objeto da classe Cachorro e faça o casting para a classe Animal.
+//package atividades.segundo;
+/// / Crie uma classe Animal e uma classe Cachorro que herda de Animal.
+/// / Em seguida, crie um objeto da classe Cachorro e faça o casting para a classe Animal.
+//
+//class Animal {
+//    // Propriedades e métodos
+//}
+//
+//class Cachorro {
+//    // Propriedades e métodos
+//}
 
-class Animal {
-    // Propriedades e métodos
-}
+//public class Main {
+//    public static void main(String[] args) {
+//        Cachorro cachorro = new Cachorro();
+//        Animal animal = cachorro;
 
-class Cachorro {
-    // Propriedades e métodos
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Cachorro cachorro = new Cachorro();
-        Animal animal = cachorro;
-
-
-    }
-}
+//
+//    }
+//}
--- src/atividades/sexto/ContaBancaria.java
+++ src/atividades/sexto/ContaBancaria.java
@@ -1,31 +1,31 @@
-package atividades.sexto;
-// Crie uma classe ContaBancaria com propriedades como número da conta e saldo. Em seguida, crie uma lista
-// de contas bancárias com diferentes saldos. Utilize um loop para encontrar e imprimir a conta com o maior saldo.
-
-import java.util.ArrayList;
-
-record ContaBancaria(int numeroConta, double saldo) {
-}
-
-public class Main {
-    public static void main(String[] args) {
-        ContaBancaria conta1 = new ContaBancaria(101, 1500.0);
-        ContaBancaria conta2 = new ContaBancaria(102, 2500.0);
-        ContaBancaria conta3 = new ContaBancaria(103, 1800.0);
-
-        ArrayList<ContaBancaria> listaContas = new ArrayList<>();
-        listaContas.add(conta1);
-        listaContas.add(conta2);
-        listaContas.add(conta3);
-
-        ContaBancaria contaMaiorSaldo = listaContas.get(0);
-        for (ContaBancaria conta : listaContas) {
-            if (conta.saldo() > contaMaiorSaldo.saldo()) {
-                contaMaiorSaldo = conta;
-            }
-        }
-
-        System.out.println("Conta com o maior saldo - Número: " + contaMaiorSaldo.numeroConta() +
-                ", Saldo: " + contaMaiorSaldo.saldo());
-    }
-}
+//package atividades.sexto;
+/// / Crie uma classe ContaBancaria com propriedades como número da conta e saldo. Em seguida, crie uma lista
+/// / de contas bancárias com diferentes saldos. Utilize um loop para encontrar e imprimir a conta com o maior saldo.
+//
+//import java.util.ArrayList;
+//
+//record ContaBancaria(int numeroConta, double saldo) {
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        ContaBancaria conta1 = new ContaBancaria(101, 1500.0);
+//        ContaBancaria conta2 = new ContaBancaria(102, 2500.0);
+//        ContaBancaria conta3 = new ContaBancaria(103, 1800.0);
+//
+//        ArrayList<ContaBancaria> listaContas = new ArrayList<>();
+//        listaContas.add(conta1);
+//        listaContas.add(conta2);
+//        listaContas.add(conta3);
+//
+//        ContaBancaria contaMaiorSaldo = listaContas.get(0);
+//        for (ContaBancaria conta : listaContas) {
+//            if (conta.saldo() > contaMaiorSaldo.saldo()) {
+//                contaMaiorSaldo = conta;
+//            }
+//        }
+//
+//        System.out.println("Conta com o maior saldo - Número: " + contaMaiorSaldo.numeroConta() +
+//                ", Saldo: " + contaMaiorSaldo.saldo());
+//    }
+//}
